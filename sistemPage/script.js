07af100b3810a4ae7c14d45cccc12335190602f2
Chocolate Code <alialfatih303@gmail.com>
2020-11-13 08:03:12 +0700

    menambahkan sistempage

---
+++ sistemPage/script.js
@@ -0,0 +1,27 @@
+// init
+AOS.init();
+
+const navigation = document.querySelector('.nav');
+const toggle = document.querySelector('.toggle');
+const close = document.querySelector('.toggle .fas');
+toggle.addEventListener('click', function(){
+navigation.classList.toggle('active');
+toggle.classList.toggle('active');
+
+});
+
+const akun = document.querySelector('.nav ul .imgAcc');
+console.log(akun);
+const emoji = document.querySelector('.nav ul .account');
+emoji.addEventListener('click', function() {
+    akun.classList.toggle('accountable');
+})
+
+
+
+
+
+
+
+
+
